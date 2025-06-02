d8c996e74854ba4a941c65b9491f5a0022180f27
Urnguk <urnguk@gmail.com>
2025-06-02 14:43:45 +0300

    multi_plot added

--- visualization.py
+++ visualization.py
@@ -1,4 +1,5 @@
 from matplotlib import pyplot as plt
+import numpy as np
 
 
 def fast_plot(x, y, domain=None, show=True):
@@ -8,6 +9,16 @@ def fast_plot(x, y, domain=None, show=True):
     if domain == "FD":
         plt.xlabel(r"$\omega$, GHz")
     plt.grid()
+    if show:
+        plt.show()
+
+
+def multi_plot(data, domains=None, n_rows=1, n_cols=2, show=True):
+    if domains is None:
+        domains = [None] * len(data)
+    for i in range(len(data)):
+        plt.subplot(n_rows, n_cols, i + 1)
+        fast_plot(*data[i], domains[i], show=False)
     if show:
         plt.show()
 
