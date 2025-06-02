bb3652e84ee4f8c057c1c5527a6d73c4d4d85df2
Urnguk <urnguk@gmail.com>
2025-06-02 11:42:44 +0300

    Create analisys.py

---
+++ analisys.py
@@ -0,0 +1,16 @@
+def eye_data(t, y, slots=100, tau=100):
+    res = []
+    for slot in range(slots - 2):
+        X, Y = [], []
+        for i in range(len(t)):
+            x = t[i] // tau
+            if slot <= x < slot + 3:
+                X.append(t[i] - slot * tau)
+                Y.append(y[i])
+        res.append((X, Y))
+    return res
+
+
+
+
+
