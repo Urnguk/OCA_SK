a1b031a9d0369dde4bd95df3062aa269443659e7
Urnguk <urnguk@gmail.com>
2025-06-04 08:46:18 +0300

    attempt at BER calculation

--- analisys.py
+++ analisys.py
@@ -1,3 +1,7 @@
+import numpy as np
+from scipy.special import erfc
+
+
 def eye_data(t, y, slots=100, tau=100):
     res = []
     for slot in range(slots - 2):
@@ -13,6 +17,19 @@ def eye_data(t, y, slots=100, tau=100):
     return res
 
 
+def calculate_ber(signal, bits, timescale, tau):
+
+
+    # Separate 0s and 1s
+    sig0 = signal[bit_idx == 0]
+    sig1 = signal[bit_idx == 1]
+
+    # Calculate statistics
+    V0, V1 = np.median(sig0), np.median(sig1)
+    sigma0, sigma1 = np.std(sig0), np.std(sig1)
+
+    Q = abs(V1 - V0) / (sigma0 + sigma1)
+    return 0.5 * erfc(Q / np.sqrt(2))
 
 
 
--- main.py
+++ main.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     slots = 100
     n_dots = 2 ** 10
 
-    timescale, signal = sg.gen_signal(slots, tau, n_dots)
+    original_seq, timescale, signal = sg.gen_signal(slots, tau, n_dots)
     omega, signal_omega = pc.fourier(signal, slots, tau, n_dots)
     lpf = pc.LPF(omega)
     new_signal = np.real(pc.inv_fft(signal_omega * lpf))
@@ -32,82 +32,28 @@ if __name__ == "__main__":
     vs.multi_plot(data, 1, 2, subroutine=vs.eye_plot)
     signal = new_signal
 
+    V_pi = 2.0  # Typical MZM parameter
+    P_optical = pc.mzm_modulate(signal)
+    vs.eye_plot(an.eye_data(timescale, P_optical, slots, tau))
 
+    V_clean, V_noisy = pc.photodetector(P_optical)
 
-
-
-
-
-    # delta_y = np.array([rm.gauss(0.5, 0.1) for i in range(n_dots)])
-    # nrz_y += delta_y * 0.1
-
-
-
-
-
+    vs.eye_plot(an.eye_data(timescale, V_noisy, slots, tau))
+    
+    # bits = original_seq
+    # samples_per_bit = n_dots // slots
+    # ber = an.calculate_ber(V_noisy, bits, samples_per_bit)
+    # print(f"Estimated BER: {ber:.2e}")
     #
-
-    # mean = slots // 2
-    # sigma = 1
-    #
-    # get_gauss = els.gauss_signal(mean, sigma)
-    # gauss_y = np.array([get_gauss(x) for x in dots])
-    # gauss_y *= np.sin(100 * np.pi * dots)
-    # gauss_w = fft.fft(gauss_y)
-
-    # plt.subplot(2, 2, 1)
-    # plt.plot(dots, gauss_y)
-    # plt.subplot(2, 2, 2)
-    # plt.plot(freqs, np.abs(gauss_w) ** 2)
-    # plt.subplot(2, 2, 3)
-    # plt.plot(dots, np.arctan(np.imag(gauss_y) / np.real(gauss_y)))
-    # plt.subplot(2, 2, 4)
-    # plt.plot(freqs, np.arctan(np.imag(gauss_w) / np.real(gauss_w)))
-    # eye_data = []
-    # for i in range(slots - 2):
-    #     X = []
-    #     Y = []
-    #     for j in range(len(dots)):
-    #         if i <= dots[j] < i + 3:
-    #             X.append(dots[j] - i)
-    #             Y.append(nrz_y[j])
-    #     eye_data.append((X, Y))
-    # for X, Y in eye_data:
-    #     plt.plot(X, Y)
-    # plt.show()
-    #
-    # n_bins = 100
-    # bins = [0 for i in range(n_bins)]
-    #
-    #
-    # for X, Y in eye_data:
-    #     for i in range(len(X)):
-    #         if 1.25 < X[i] < 1.75 and Y[i] > 0.55:
-    #             for j in range(n_bins):
-    #                 if 0.1 * j / n_bins <= Y[i] - 1 < 0.1 * (j + 1) / n_bins:
-    #                     bins[j] += 1
-    #
-    # bins = np.array(bins) / np.sum(bins)
-    # bin_steps = np.array([i / n_bins for i in range(n_bins)])
-    # popt, pcov = curve_fit(gauss_func, bin_steps, bins, bounds = (0, 1))
-    # gauss_fit = [gauss_func(t, *popt) for t in bin_steps]
-    # plt.plot(bin_steps, bins)
-    # plt.plot(bin_steps, gauss_fit, color="red")
-    # print(*popt)
-    # plt.show()
-
-    # for i in range(len(delta_y)):
-    #     for j in range(n_bins):
-    #         if j / n_bins <= delta_y[i] < (j + 1) / n_bins:
-    #             bins[j] += 1
-    # bins = np.array(bins) / 1024
-    # bin_steps = np.array([i / n_bins for i in range(n_bins)])
-    # popt, pcov = curve_fit(gauss_func, bin_steps, bins, bounds = (0, 1))
-    # gauss_fit = [gauss_func(t, *popt) for t in bin_steps]
-    # plt.plot(bin_steps, bins)
-    # plt.plot(bin_steps, gauss_fit, color="red")
-    # print(*popt)
-    # plt.show()
+    # # 4. Visualization
+    # data = [
+    #     (timescale, signal, "TD"),
+    #     (timescale, P_optical, "TD"),
+    #     (timescale, V_noisy, "TD"),
+    #     an.eye_data(timescale, V_noisy, slots, tau)
+    # ]
+    # vs.multi_plot(data[:3], n_rows=3, n_cols=1)
+    # vs.eye_plot(data[3])
 
 
 
